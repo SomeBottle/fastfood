34e34e463b864b558c723ca22707711c948127c4
SomeBottle <somebottle@outlook.com>
2025-08-13 01:24:56 +0800

    feat: add AutoDL JupyterLab URL Copier

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 这里放一些咱写的自用脚本或者页面 🍕
 
 ## 本仓库的脚本可能会出现以下情况
+
 * 随心所欲的写，越浪越开心<del>（能用就行）</del>
 * 可维护性低
 * 待更新
@@ -15,10 +16,25 @@
 
 * [整活型GPA计算工具(适用于WHPU正方教务系统)](#整活型gpa计算工具)
 
+* [AutoDL JupyterLab URL 复制按钮（油猴脚本）]
+
 ## 页面目录
 
-* [逃跑的颜文字](kaomoji.html)  
+* [逃跑的颜文字](kaomoji.html) - 有个域名但是不知道页面放什么时就用这个吧。  
 
-## 整活型GPA计算工具
+## 脚本说明
+
+### 整活型GPA计算工具
 
 具体见GreasyForks上的页面： [点我前往](https://greasyfork.org/zh-CN/scripts/440188-%E6%95%B4%E6%B4%BB%E5%9E%8Bgpa%E8%AE%A1%E7%AE%97%E5%B7%A5%E5%85%B7-%E9%80%82%E7%94%A8%E4%BA%8Ewhpu%E6%AD%A3%E6%96%B9%E6%95%99%E5%8A%A1%E7%B3%BB%E7%BB%9F)  
+
+### AutoDL JupyterLab URL 复制按钮
+
+![example](./pics/jupyterLabUrlCopier/example.png)  
+
+主要用于在 AutoDL 控制台快速复制 JupyterLab 的 URL (带 token)，以便在 VSCode 中进行连接（苦逼做实验中 ... T_T）  
+
+> 通过拦截页面中 `XMLHttpRequest` 的响应来实现，因为 AutoDL 会轮询一个 `/api/v1/instance` 接口来更新列表信息。如果没有按钮可以尝试刷新一下实例列表。  
+
+* [autodl-jupyterlab-url-copier.js](./autodl-jupyterlab-url-copier.js)  
+
---
+++ autodl-jupyterlab-url-copier.js
@@ -0,0 +1,135 @@
+// ==UserScript==
+// @name         AutoDL JupyterLab URL Copier
+// @namespace    https://gist.github.com/SomeBottle
+// @version      2025-08-12
+// @description  Copy JupyterLab URL from AutoDL instance console
+// @author       SomeBottle
+// @match        https://www.autodl.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=autodl.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    // 本脚本的前缀
+    const SCRIPT_PREFIX = '[JupyterLab URL Copier]';
+    // AutoDL 的请求基于 XMLHttpRequest, 拦截改写
+    const INSTANCE_API_URL_PATTERN = /\/api\/.*?\/instance$/i;
+    // 复制按钮的样式
+    const COPY_BTN_STYLE_SHEET = `
+        .jupyter-url-copy-btn{
+            position: relative;
+            display: block;
+            color: #46a0ffff;
+            text-decoration: none;
+            transition: .5s ease;
+        }
+        .jupyter-url-copy-btn:hover{
+            color: #1cced7ff;
+        }
+        .jupyter-url-copy-btn::after{
+            position: absolute;
+            z-index: 900;
+            content: "(๑‾ ꇴ ‾๑) 已复制";
+            left: 0;
+            top: 50%;
+            width: 100%;
+            text-align: left;
+            pointer-events: none;
+            transform: translateY(-50%);
+            transition: .5s ease;
+            opacity: 0;
+            background-color: #ffffffbf;
+        }
+        .copied-notice::after{
+            opacity: 1;
+        }
+    `;
+    // 存储实例 UUID 到 JupyterLab URL 的映射
+    const instanceId2Url = {};
+    // 存储实例 UUID 到 DOM 节点的映射
+    const instanceId2Node = {};
+    // 存储上一次拦截结果中出现的 URL
+    let prevInstanceUrls = [];
+    // 哨兵 DOM 元素，用于检测页面中是否有按钮存在
+    let sentryNode = null;
+    // 比较数组是否完全一致
+    const arraysEqual = function (a, b) {
+        if (a.length !== b.length) return false;
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] !== b[i]) return false;
+        }
+        return true;
+    };
+    // 找到页面中 instanceId 出现的元素
+    const searchNodes = function (node) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            let nodeText = node.innerText;
+            if (nodeText) {
+                nodeText = nodeText.trim();
+                if (nodeText in instanceId2Url) {
+                    instanceId2Node[nodeText] = node;
+                }
+            }
+        }
+        node.childNodes.forEach(child => searchNodes(child));
+    };
+    // 拦截 XMLHttpRequest 请求响应
+    const requestHook = function () {
+        const response = JSON.parse(this.responseText);
+        if (response.data) {
+            const instanceList = response.data.list;
+            let currentInstanceUrls = [];
+            for (let instance of instanceList) {
+                const url = `https://${instance.jupyter_domain}/jupyter/?token=${instance.jupyter_token}`;
+                currentInstanceUrls.push(url);
+                instanceId2Url[instance.uuid] = url;
+            }
+            if (sentryNode !== null && document.contains(sentryNode) && arraysEqual(currentInstanceUrls, prevInstanceUrls)) {
+                // 如果当前实例与上一次相同，则不需要更新，减少资源浪费
+                console.log(`${SCRIPT_PREFIX} No changes in instance URLs.`);
+                return;
+            }
+            prevInstanceUrls = currentInstanceUrls;
+            // 搜索 DOM 中相应的结点
+            searchNodes(document.body);
+            for (let insId in instanceId2Url) {
+                let insNode = instanceId2Node[insId];
+                // data-modified 用于防止元素重复添加
+                if (insNode && insNode.getAttribute('data-modified') != "true") {
+                    insNode.setAttribute('data-modified', 'true');
+                    // 在这个结点的后面添加一个复制 URL 的按钮
+                    let copyBtn = document.createElement('a');
+                    copyBtn.innerText = '复制 JupyterLab URL';
+                    copyBtn.href = 'javascript:void(0)';
+                    copyBtn.onclick = function () {
+                        navigator.clipboard.writeText(instanceId2Url[insId]);
+                        // 复制后的提示
+                        this.classList.add('copied-notice');
+                        setTimeout(() => {
+                            this.classList.remove('copied-notice');
+                        }, 1200);
+                    };
+                    copyBtn.classList.add('jupyter-url-copy-btn');
+                    sentryNode = copyBtn;
+                    insNode.parentNode.insertAdjacentElement('afterend', copyBtn);
+                    console.log(`${SCRIPT_PREFIX} Added copy button of the instance: ${insId}`);
+                }
+            }
+        }
+        this.removeEventListener('load', requestHook);
+    };
+    const originalOpen = XMLHttpRequest.prototype.open;
+    window.XMLHttpRequest.prototype.open = function (method, url, ...args) {
+        if (INSTANCE_API_URL_PATTERN.test(url)) {
+            // 如果是指定 URL 则开始监听
+            this.addEventListener('load', requestHook.bind(this));
+        }
+        return originalOpen.apply(this, [method, url, ...args]);
+    };
+    // 把按钮样式加入 DOM
+    const btnStyle = document.createElement('style');
+    btnStyle.innerHTML = COPY_BTN_STYLE_SHEET;
+    document.head.appendChild(btnStyle);
+    console.log(`${SCRIPT_PREFIX} JupyterLab URL Copier Loaded, have fun~ /ᐠ｡ꞈ｡ᐟ\\`);
+})();
