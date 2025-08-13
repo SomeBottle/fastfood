d59c584b8bf978789b477cd115845eda19625ec0
SomeBottle <somebottle@outlook.com>
2025-08-13 19:31:07 +0800

    fix(autodl-jupyterlab-url-copier): btn life cycle and url construct logic
    1. Fix the issue where copier buttons are not removed in a timely manner due to the lack of lifecycle management.  
    2. Fix the JupyterLab URL construction logic.  
    3. Fix the issue where nodes removed from the document still remained in instanceId2Node.

--- autodl-jupyterlab-url-copier.js
+++ autodl-jupyterlab-url-copier.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoDL JupyterLab URL Copier
 // @namespace    https://github.com/SomeBottle/fastfood
-// @version      2025-08-13
-// @description  在 AutoDL 控制台实例列表显示 JupyterLab URL 复制按钮
+// @version      2025-08-13-02
+// @description  在 AutoDL 控制台实例列表显示 JupyterLab URL 复制按钮 (含 token)
 // @author       SomeBottle
 // @match        https://www.autodl.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=autodl.com
@@ -82,12 +82,16 @@
             const instanceList = response.data.list;
             let currentInstanceUrls = [];
             for (let instance of instanceList) {
-                const url = `https://${instance.jupyter_domain}/jupyter/?token=${instance.jupyter_token}`;
+                let url = `https://${instance.jupyter_domain}/jupyter/?token=${instance.jupyter_token}`;
+                if (instance.jupyter_port !== 0) {
+                    // 根据 AutoDL 的接口前端处理逻辑，如果 jupyter_port 不为 0，则采用 proxy_host + port，且为 http 协议
+                    url = `http://${instance.proxy_host}:${instance.jupyter_port}/jupyter/?token=${instance.jupyter_token}`;
+                }
                 currentInstanceUrls.push(url);
                 instanceId2Url[instance.uuid] = url;
             }
             if (sentryNode !== null && document.contains(sentryNode) && arraysEqual(currentInstanceUrls, prevInstanceUrls)) {
-                // 如果当前实例与上一次相同，则不需要更新，减少资源浪费
+                // 如果当前实例 URL 与上一次相同，则不需要更新，减少资源浪费
                 console.log(`${SCRIPT_PREFIX} No changes in instance URLs.`);
                 return;
             }
@@ -97,26 +101,51 @@
             for (let insId in instanceId2Url) {
                 let insNode = instanceId2Node[insId];
                 let copyBtnId = `jupyter-url-copy-btn-${insId}`;
+                if (!insNode) {
+                    // 如果节点不存在，则跳过
+                    continue;
+                }
+                if (!document.contains(insNode)) {
+                    // 如果节点不在文档中，则跳过，并移除掉映射
+                    // 这种情况发生在用户释放了实例，页面中项目消失，但是 instanceId2Node 还存着
+                    delete instanceId2Node[insId];
+                    continue;
+                }
                 // 防止元素重复添加
-                if (insNode && document.getElementById(copyBtnId) === null) {
+                let btnElement = document.getElementById(copyBtnId);
+                if (btnElement === null) {
                     // 在这个结点的后面添加一个复制 URL 的按钮
                     let copyBtn = document.createElement('a');
                     copyBtn.innerText = '复制 JupyterLab URL';
                     copyBtn.href = 'javascript:void(0)';
-                    copyBtn.onclick = function () {
-                        navigator.clipboard.writeText(instanceId2Url[insId]);
-                        // 复制后的提示
-                        this.classList.add('copied-notice');
-                        setTimeout(() => {
-                            this.classList.remove('copied-notice');
-                        }, 1200);
-                    };
                     copyBtn.classList.add('jupyter-url-copy-btn');
                     copyBtn.id = copyBtnId;
                     sentryNode = copyBtn;
-                    insNode.parentNode.insertAdjacentElement('afterend', copyBtn);
+                    insNode.insertAdjacentElement('afterend', copyBtn);
                     console.log(`${SCRIPT_PREFIX} Added copy button of the instance: ${insId}`);
+                    btnElement = copyBtn;
+                    // 因为按钮的生命周期不受 AutoDL 前端管理（比如删除实例项后按钮还在），需要手动管理
+                    // 在对应项目消失时，按钮一并消失
+                    (function (btn, insNode, insId) {
+                        let observer = new MutationObserver(() => {
+                            // 元素可能被移除，也可能是内容被替换
+                            if (!document.contains(insNode) || insNode.innerText.trim() !== insId) {
+                                btn.remove();
+                                observer.disconnect();
+                            }
+                        });
+                        observer.observe(insNode.parentNode, { childList: true });
+                    })(btnElement, insNode, insId);
                 }
+                // 因为 URL 变动了，就算元素存在，也要更新 URL
+                btnElement.onclick = function () {
+                    navigator.clipboard.writeText(instanceId2Url[insId]);
+                    // 复制后的提示
+                    this.classList.add('copied-notice');
+                    setTimeout(() => {
+                        this.classList.remove('copied-notice');
+                    }, 1200);
+                };
             }
         }
         this.removeEventListener('load', requestHook);
